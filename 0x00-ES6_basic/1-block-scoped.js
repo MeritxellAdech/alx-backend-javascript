c0db4e8b120f4dc86891e796f8963bc8b821465c
Merit <meritxelladech@gmail.com>
2024-10-02 11:22:37 +0100

    Using constants to overcoming hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 // Preventing the variables from being overriden
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
