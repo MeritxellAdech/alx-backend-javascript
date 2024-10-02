451cee702f20ae15d118e67ef662fa28f0aa80c9
Merit <meritxelladech@gmail.com>
2024-10-02 11:28:36 +0100

    Rewriting a function as arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+// using an anonymous or arrow function
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = ((newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  });
+}
