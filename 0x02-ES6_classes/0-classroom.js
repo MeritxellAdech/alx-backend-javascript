de12c277d92186a720e428ad4c3a2619ac774984
Merit <meritxelladech@gmail.com>
2024-10-03 20:17:49 +0100

    Create a classroom class and initialize the number of students

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// count the number of students found in a class
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
