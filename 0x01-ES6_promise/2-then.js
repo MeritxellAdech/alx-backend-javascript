358260d4a7db2d05954cf40a195ac241ddc4ecc5
Merit <meritxelladech@gmail.com>
2024-10-13 22:09:55 +0100

    promise that logs a message regardless the response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,10 @@
-//
+// This module create a promise that logs a message regardless the response
 
-export default function handleResponseFromAPI(promise) {
-  promise.then(
-    (res) => res,
-  ).catch(() => new Error()).finally(() => {
-    console.log('Got a response from the API');
-  });
+export default async function handleResponseFromAPI(promise) {
+  return promise.then(
+    () => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    },
+  ).catch(() => new Error());
 }
