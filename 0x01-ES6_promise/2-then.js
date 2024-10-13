cd180fd525e22830caff7ef311dec48cfd97a36e
Merit <meritxelladech@gmail.com>
2024-10-13 22:09:47 +0100

    promise that logs a message regardless the response

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+//
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(
+    (res) => res,
+  ).catch(() => new Error()).finally(() => {
+    console.log('Got a response from the API');
+  });
+}
