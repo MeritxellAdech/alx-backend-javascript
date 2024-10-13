15d897731a9c26a579c83e63fd63fe44e4ec984c
Merit <meritxelladech@gmail.com>
2024-10-13 21:39:20 +0100

    Using the callback functions

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+// This module creates an object of a promise that make use of its callbacks
+/* eslint-disable no-unused-vars */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      return { status: 200, body: 'Success' };
+    }
+    throw new Error('The fake API is not working currently');
+  });
+}
